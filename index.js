52b59b466062c1b63d8f08f47f6477d989b11c5c
Musa Siddeeq <musa.siddeeq@turner.com>
2018-04-14 13:41:36 -0700

    fix: path module

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const rp = require('request-promise');
 const twilio = require('twilio');
+const path = require('path');
 
 const client = new twilio(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
 const app = express();
